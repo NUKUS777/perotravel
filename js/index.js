ba672623dafb4fa1394fe1e6f4d41b047d1532df
Dawlet Kenesbaev <123051703+NUKUS777@users.noreply.github.com>
2023-03-22 15:34:25 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector(".navbar__menu");
+const hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener('click',()=>{
+    menu.classList.toggle('active')
+    hamburger.classList.toggle('active')
+})
